86f2122907a376b9deb2fa6683656cadaafaa8d2
Muntasir2001 <muntasirenamalhuda@gmail.com>
2023-04-26 10:40:32 +0100

    updated the port number

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
+import os
 import http.server, ssl, socketserver
+
+PORT=3000
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 
 context.load_cert_chain("cert.pem")
-port=5000
-server_address = ("0.0.0.0", port) 
+server_address = ("0.0.0.0", PORT)
 
 handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(server_address, handler) as httpd:
     httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
-    print(f"Server listening on https://localhost:{port}")
+    print(f"Server listening on https://localhost:{PORT}")
     httpd.serve_forever()
